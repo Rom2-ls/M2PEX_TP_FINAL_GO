9d1f8dd53e5923dde5c7e28e4579d0db5cfec2a5
Romain Capolunghi <capolunghiromain@gmail.com>
2025-06-23 10:24:39 +0200

    fix link struct

--- internal/models/link.go
+++ internal/models/link.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 // TODO : Créer la struct Link
 // Link représente un lien raccourci dans la base de données.
 // Les tags `gorm:"..."` définissent comment GORM doit mapper cette structure à une table SQL.
@@ -12,5 +14,5 @@ type Link struct {
 	ID        uint   `gorm:"primaryKey"`        // Clé primaire
 	Shortcode string `gorm:"uniqueIndex;size:10"` // Code court unique, indexé pour des recherches rapides, taille maximale de 10 caractères
 	LongURL   string `gorm:"not null"` // URL longue, ne peut pas être nulle
-	CreatedAt int64 `gorm:"autoCreateTime"` // Horodatage de création, automatiquement défini par GORM
+	CreatedAt time.Time `gorm:"autoCreateTime"` // Horodatage de création, automatiquement défini par GORM
 }
